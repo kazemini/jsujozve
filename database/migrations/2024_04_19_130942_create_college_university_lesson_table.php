27c9b2c7c4fc5416dbfa94e869b7cfcef1190f46
Ali Kazemini <kazemini.business@gmail.com>
2024-04-19 17:06:26 +0330

    college university lesson table

---
+++ database/migrations/2024_04_19_130942_create_college_university_lesson_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('college_university_lesson', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('college_university_id')->constrained()->onUpdate('cascade')->onDelete('restrict');
+            $table->foreignId('lesson_id')->constrained()->onUpdate('cascade')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('college_university', function (Blueprint $table) {
+            $table->dropForeign('college_university_id');
+        });
+
+        Schema::table('lessons', function (Blueprint $table) {
+            $table->dropForeign('lesson_id');
+        });
+
+        Schema::dropIfExists('college_university_lesson');
+    }
+};
