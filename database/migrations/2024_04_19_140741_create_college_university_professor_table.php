60584ebfc113f0c89971b91ccdaf014af11de447
Ali Kazemini <kazemini.business@gmail.com>
2024-04-19 17:40:05 +0330

    college university professor table

---
+++ database/migrations/2024_04_19_140741_create_college_university_professor_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('college_university_professor', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('professor_id')->constrained()->onUpdate('cascade')->onDelete('restrict');
+            $table->foreignId('college_university_id')->constrained()->onUpdate('cascade')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('professors', function (Blueprint $table) {
+            $table->dropForeign('professor_id');
+        });
+        Schema::table('college_university', function (Blueprint $table) {
+            $table->dropForeign('college_university_id');
+        });
+
+        Schema::dropIfExists('college_university_professor');
+    }
+};
