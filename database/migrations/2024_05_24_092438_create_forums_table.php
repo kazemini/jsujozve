7b830ba5e274644376c10341a0a32e97b946e47e
Ali Kazemini <kazemini.business@gmail.com>
2024-05-24 13:09:56 +0330

    create forums table migration

---
+++ database/migrations/2024_05_24_092438_create_forums_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('forums', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',100);
+            $table->string('description',1000);
+            $table->foreignId('admin_id');
+            $table->foreign('admin_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('admin_id');
+        });
+        Schema::dropIfExists('forums');
+    }
+};
