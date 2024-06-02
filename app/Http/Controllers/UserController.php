039a46c830dd413b0358f10fa523e74a74616b5b
Ali Kazemini <kazemini.business@gmail.com>
2024-06-02 19:24:11 +0330

    user controller, logout function

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class UserController extends Controller
+{
+    public function logout() {
+        Auth::logout();
+
+        return Redirect::to('/');
+    }
+}
