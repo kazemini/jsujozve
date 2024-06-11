fb30482a395131e63291981c270c58493cf839de
Ali Kazemini <kazemini.business@gmail.com>
2024-06-11 17:57:48 +0330

    create and manage document routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CreateDocumentController;
+use App\Http\Controllers\DocumentManagementController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -13,7 +15,8 @@ Auth::routes();
 Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 Route::get('/logout', [ProfileController::class, 'logout'])->name('profile.logout');
-
+Route::get('/create_document',[CreateDocumentController::class,'index'])->name('document.create');
+Route::get('/manage_document',[DocumentManagementController::class,'index'])->name('document.management');
 Route::get('/test', function (){
     return view('overview');
 })->name('test');
