01b487043a8b1473a24fb2b21012a64e2ed4e96d
Ali Kazemini <kazemini.business@gmail.com>
2024-06-12 19:39:04 +0330

    a gate to register for manage-document

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Document;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('manage-document', function (User $user, Document $document) {
+            return $user->id === $document->author_id;
+        });
     }
 }
