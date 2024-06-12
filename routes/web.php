911310def3645ef2a9b0b7e552963d79249cc387
Ali Kazemini <kazemini.business@gmail.com>
2024-06-12 11:48:24 +0330

    destroy document route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CreateDocumentController;
 use App\Http\Controllers\DocumentManagementController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\CheckAuthor;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -12,12 +13,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-Route::get('/logout', [ProfileController::class, 'logout'])->name('profile.logout');
-Route::get('/document',[CreateDocumentController::class,'index'])->name('document.index');
-Route::post('/document/create',[CreateDocumentController::class,'create'])->name('document.create');
-Route::get('/document/manage',[DocumentManagementController::class,'index'])->name('document.management');
-Route::get('/test', function (){
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile')->middleware('auth');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update')->middleware('auth');
+Route::get('/logout', [ProfileController::class, 'logout'])->name('profile.logout')->middleware('auth');
+Route::get('/document', [CreateDocumentController::class, 'index'])->name('document.index')->middleware('auth');
+Route::post('/document/create', [CreateDocumentController::class, 'create'])->name('document.create')->middleware('auth');
+Route::get('/document/manage', [DocumentManagementController::class, 'index'])->name('document.management')->middleware('auth');
+Route::delete('/document/{document}', [DocumentManagementController::class, 'destroy'])->name('document.delete')->middleware('auth', CheckAuthor::class);
+Route::get('/test', function () {
     return view('overview');
 })->name('test');
