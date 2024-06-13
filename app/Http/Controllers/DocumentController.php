46208de4906aa7855c5ae16a96248c07d42c9fb4
Ali Kazemini <kazemini.business@gmail.com>
2024-06-13 22:48:01 +0330

    edit a document by an editor user

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -34,4 +34,28 @@ class DocumentController extends Controller
 
         return redirect()->back();
     }
+
+    public function edit(Document $document)
+    {
+        return view('document_public_edit', ['document' => $document->load(
+            ['logs' => function ($query) {
+                $query->latest('created_at')->take(1);
+            }]
+        )]);
+    }
+
+    public function update(Request $request, Document $document)
+    {
+        $document->logs()->create([
+            'title' => $request['newTitle'],
+            'description' => $request['newDescription'],
+            'university' => $request['newUniversity'],
+            'department' => $request['newDepartment'],
+            'professor' => $request['newProfessor'],
+            'lesson' => $request['newLesson'],
+            'editor_id' => auth()->user()->id,
+        ]);
+
+        return redirect()->route('document.explore')->with(["status" => "با موفقیت ویرایش شد"]);
+    }
 }
