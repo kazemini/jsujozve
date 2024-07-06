a97b93a92435f255167d1e1eb26d253a1da2ee1e
Ali Kazemini <kazemini.business@gmail.com>
2024-07-06 17:36:47 +0330

    forum model relations

--- app/Models/Forum.php
+++ app/Models/Forum.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Forum extends Model
 {
@@ -14,4 +16,19 @@ class Forum extends Model
         'description',
         'admin_id'
     ];
+
+    public function admin(): BelongsTo
+    {
+        return $this->belongsTo(User::class,'admin_id');
+    }
+
+    public function subscribers(): HasMany
+    {
+        return $this->hasMany(Follow::class)->with('user');
+    }
+
+    public function subscribed(): HasMany
+    {
+        return $this->hasMany(Follow::class)->where( 'user_id',auth()->user()->id)->take(1);
+    }
 }
