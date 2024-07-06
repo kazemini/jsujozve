1fa77595692a341bf6784c6667ce90a157041acd
Ali Kazemini <kazemini.business@gmail.com>
2024-07-06 17:36:30 +0330

    user HasMany Forums relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Document::class);
     }
+
+    public function forums(): HasMany
+    {
+        return $this->hasMany(Forum::class);
+    }
 }
